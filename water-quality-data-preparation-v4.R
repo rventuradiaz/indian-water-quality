dccf22a7939071418763304209b81af1aaa2516a
rventuradiaz <rventura@gmx.net>
2017-02-04 16:16:40 +0100

    create a new function for df preparation

--- water-quality-data-preparation-v4.R
+++ water-quality-data-preparation-v4.R
@@ -67,3 +67,65 @@ tab_ready[is.nan(tab_ready)] <- 0
 rownames(tab_ready)<-as.character(wq1s[,"VillageName"])
 return(tab_ready)
 }
+
+wq1_df <- function(wq1){
+  require(devtools)
+  require(cluster)
+  wq1$QualityParameter = as.character(wq1$QualityParameter)
+  wq1$Year <- as.character(wq1$Year)
+  wq1$VillageName<- as.character(paste(wq1$VillageName, wq1$Year, sep = "-"))
+  wq1["Arsenic"] <- 0
+  wq1["Arsenic"] <- as.numeric(grepl("Arsenic",wq1$QualityParameter))*1
+  wq1["Fluoride"] <- 0
+  wq1["Fluoride"] <- as.numeric(grepl("Fluoride",wq1$QualityParameter))*2
+  wq1["Iron"] <- 0
+  wq1["Iron"] <- as.numeric(grepl("Iron",wq1$QualityParameter))*3
+  wq1["Nitrate"] <- 0
+  wq1["Nitrate"] <- as.numeric(grepl("Nitrate",wq1$QualityParameter))*4
+  wq1["Salinity"] <- 0
+  wq1["Salinity"] <- as.numeric(grepl("Salinity",wq1$QualityParameter))*5
+  wqArsenic <- aggregate(Arsenic ~ StateName +
+                           DistrictName +
+                           BlockName +
+                           PanchayatName +
+                           VillageName +
+                           HabitationName +
+                           Year,
+                         data= wq1, sum )
+  wqFlouride <- aggregate(Fluoride ~ StateName +
+                            DistrictName +
+                            BlockName +
+                            PanchayatName +
+                            VillageName +
+                            HabitationName +
+                            Year,
+                          data = wq1, sum )
+  wqIron <- aggregate(Iron ~ StateName +
+                        DistrictName +
+                        BlockName +
+                        PanchayatName +
+                        VillageName +
+                        HabitationName +
+                        Year, data=wq1, sum )
+  wqNitrate <- aggregate(Nitrate ~ StateName +
+                           DistrictName +
+                           BlockName +
+                           PanchayatName +
+                           VillageName +
+                           HabitationName +
+                           Year, data=wq1, sum )
+  wqSalinity <- aggregate(Salinity ~ StateName +
+                            DistrictName +
+                            BlockName +
+                            PanchayatName +
+                            VillageName +
+                            HabitationName +
+                            Year, data=wq1, sum )
+  wq1s <- merge(wqArsenic, wqFlouride)
+  wq1s <- merge(wq1s, wqIron)
+  wq1s <- merge(wq1s, wqNitrate)
+  wq1s <- merge(wq1s, wqSalinity)
+  wq1s["All"] <- wq1s[,8]+wq1s[,9]++wq1s[,10] +wq1s[,11] +wq1s[,12]  
+  wq1s <- wq1s[order(wq1s[,4], wq1s[,1], wq1s[2],  wq1s[3]), ]
+  return(wq1s)
+}
