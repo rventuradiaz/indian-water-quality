ee16a5f24420a5294d5b63f96a533bc0a9f49cf6
rventuradiaz <rventura@gmx.net>
2017-03-13 17:23:27 +0100

    change 5

--- water-quality-data-preparation-v4.R
+++ water-quality-data-preparation-v4.R
@@ -87,6 +87,12 @@ wq1_df <- function(wq1){
   return(wq1)
 }
 
-draw_hm <- function(data, x, y, fillInput, facetInput, fill_discrete="QualityParameter") {
+draw_hm <- function(data, 
+                    x, 
+                    y, 
+                    fillInput, 
+                    facetInput, 
+                    fill_discrete="QualityParameter"){
   library(ggplot2)
   ggplot(data, aes(x, y))+geom_raster(aes(fill = fillInput))+labs(title ="Heat Map", x = "Year", y = "Quality Parameter", cey=0.8)+scale_fill_discrete(name = fill_discrete)+facet_grid(facets =  . ~ facetInput)
+}
